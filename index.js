480ad74f22e5a7286e961f8ca5cfc317daf645a5
CorpulentBrony <corpulent@brony.email>
2023-05-13 15:28:13 -0400

    Update index.js
    Minor change to polyfill

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ import { SmuttyApp } from "./SmuttyApp.mjs";
 // <!--# endif -->
 
 if (!self.requestAnimationFrame)
-	self.requestAnimationFrame = self.webkitRequestAnimationFrame || self.mozRequestAnimationFrame || self.msRequestAnimationFrame || self.oRequestAnimationFrame || function(callback) { self.setTimeout(callback); };
+	self.requestAnimationFrame = self.webkitRequestAnimationFrame || self.mozRequestAnimationFrame || self.msRequestAnimationFrame || self.oRequestAnimationFrame || self.setTimeout;
 
 (function() {
 	// <!--# if expr="$isModuleLoad = false" -->
 		// <!--# include file="/modules/SmuttyConstants.mjs" -->
+		// <!--# include file="/modules/SmuttyInstaller.mjs" -->
 		// <!--# include file="/modules/SmuttyApp.mjs" -->
 		// <!--# include file="/modules/SmuttyFileUpload.mjs" -->
 		// <!--# include file="/modules/SmuttyInteractionHandler.mjs" -->
