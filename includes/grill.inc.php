f1914de3c06c2c3447c668f46d2a8cca98cc1639
CorpulentBrony <corpulent@brony.email>
2023-05-13 15:18:56 -0400

    Updating grill to twibooru.org
    Changed source of grill.php from derpibooru to twibooru

--- includes/grill.inc.php
+++ includes/grill.inc.php
@@ -1,9 +1,10 @@
 <?php
 	ini_set("zlib.output_compression", 0);
 
-	const DERPIBOORU_URL = "https://derpibooru.org";
-	const GALLERIES_PATH = "/galleries/Corpulent+Brony.json?include_images=true";
-	const GALLERY_ID = 9109;
+	const DERPIBOORU_URL = "https://twibooru.org";
+	const DERPIBOORU_CDN = "https://cdn.twibooru.org/img/";
+	const GALLERIES_PATH = "/galleries.json?user=Twifag&include_images=true";
+	const GALLERY_ID = 38;
 	const TTL = 600;
 
 	function getGrill($sizes, bool $includeSource = false) {
@@ -14,13 +15,15 @@
 			$galleries = json_decode(file_get_contents($galleryListUrl), true);
 			return $galleries[array_search(GALLERY_ID, array_column($galleries, "id"))];
 		}, TTL);
-		$imageId = strval($gallery["image_ids"][array_rand($gallery["image_ids"])]);
+		// var_dump($gallery);
+		$imageId = strval($gallery["post_ids"][array_rand($gallery["post_ids"])]);
 		$image = apcu_entry("pomf_grill_{$imageId}", function() use($imageId): array {
 			return json_decode(file_get_contents(DERPIBOORU_URL . "/${imageId}.json"), true);
 		});
 		$result = array_map(function(string $size) use ($image): string {
-			$imageUrl = str_replace("//derpicdn.net/img/", "", $image["representations"][$size]);
-			return "/img/grill/{$imageUrl}";
+			// $imageUrl = str_replace(DERPIBOORU_CDN, "", $image["representations"][$size]);
+			// return "/img/grill/{$imageUrl}";
+			return $image["representations"][$size];
 		}, $sizes);
 
 		if ($includeSource) {
